bb484a8e518862544c7c9eacb69f57dc786aaf1d
philschmid <schmidphilipp1995@gmail.com>
2024-08-24 12:25:51 +0200

    wip public both

--- aws-cdk-ecs/lib/aws-cdk-ecs-stack.ts
+++ aws-cdk-ecs/lib/aws-cdk-ecs-stack.ts
@@ -3,11 +3,7 @@ import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as logs from 'aws-cdk-lib/aws-logs';
-import * as iam from 'aws-cdk-lib/aws-iam'; // Import IAM module
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-
-// cdk deploy --parameters OpenWebUiEcsCdkStack:SageMakerEndpointName=meta-llama-3-8b-instruct
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class OpenWebUiEcsCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -20,7 +16,7 @@ export class OpenWebUiEcsCdkStack extends cdk.Stack {
 
     // Create a VPC
     const vpc = new ec2.Vpc(this, 'Vpc', {
-      maxAzs: 2, // Default is all AZs in region
+      maxAzs: 2,
     });
 
     // Create an ECS cluster
@@ -47,28 +43,24 @@ export class OpenWebUiEcsCdkStack extends cdk.Stack {
       ],
     }));
 
-    // Define the Web UI container
-    const webUITaskDef = new ecs.FargateTaskDefinition(this, 'WebUITaskDef');
-    const webUIContainer = webUITaskDef.addContainer('WebUiAppContainer', {
-      image: ecs.ContainerImage.fromRegistry("ghcr.io/open-webui/open-webui:main"),
-      memoryLimitMiB: 512,
-      environment: {
-        "WEBUI_AUTH": "False",
-        "OPENAI_API_KEY": " 0p3n-w3bu!"
-      },
-      logging: new ecs.AwsLogDriver({
-        logGroup,
-        streamPrefix: 'WebUI',
-      }),
+    // Create Security Groups
+    const webUISecurityGroup = new ec2.SecurityGroup(this, 'WebUISecurityGroup', {
+      vpc,
+      description: 'Allow traffic to Web UI service',
+      allowAllOutbound: true,
     });
+    webUISecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8080), 'Allow HTTP traffic to Web UI');
 
-    webUIContainer.addPortMappings({
-      containerPort: 8080, // The Web UI listens on port 8080
+    const pipelinesBackendSecurityGroup = new ec2.SecurityGroup(this, 'PipelinesBackendSecurityGroup', {
+      vpc,
+      description: 'Allow traffic to Pipelines Backend service',
+      allowAllOutbound: true,
     });
+    pipelinesBackendSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(9099), 'Allow HTTP traffic to Pipelines Backend');
 
     // Define the Pipelines Backend container
     const pipelinesBackendTaskDef = new ecs.FargateTaskDefinition(this, 'PipelinesBackendTaskDef', {
-      taskRole: pipelinesBackendRole // Attach the IAM role to the task definition
+      taskRole: pipelinesBackendRole,
     });
 
     const pipelinesBackendContainer = pipelinesBackendTaskDef.addContainer('PipelinesBackendContainer', {
@@ -76,7 +68,7 @@ export class OpenWebUiEcsCdkStack extends cdk.Stack {
       memoryLimitMiB: 512,
       environment: {
         "SAGEMAKER_ENDPOINT_NAME": sageMakerEndpointName.valueAsString,
-        "PIPELINES_URLS": "https://raw.githubusercontent.com/philschmid/open-webui-sagemaker-example/main/pipelines/aws_sagemaker_pipeline.py"
+        "PIPELINES_URLS": "https://raw.githubusercontent.com/philschmid/open-webui-sagemaker-example/main/pipelines/aws_sagemaker_pipeline.py",
       },
       logging: new ecs.AwsLogDriver({
         logGroup,
@@ -88,27 +80,62 @@ export class OpenWebUiEcsCdkStack extends cdk.Stack {
       containerPort: 9099,
     });
 
-    // Create Fargate services
+    // Create the Pipelines Backend service and attach it to the load balancer
+    const pipelinesBackendService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'PipelinesBackendService', {
+      cluster,
+      serviceName: 'pipelines-backend',
+      taskDefinition: pipelinesBackendTaskDef,
+      publicLoadBalancer: true,
+      listenerPort: 80,
+      securityGroups: [pipelinesBackendSecurityGroup],
+    });
+
+
+    // Define the Web UI container
+    const webUITaskDef = new ecs.FargateTaskDefinition(this, 'WebUITaskDef');
+    const webUIContainer = webUITaskDef.addContainer('WebUiAppContainer', {
+      image: ecs.ContainerImage.fromRegistry("ghcr.io/open-webui/open-webui:main"),
+      memoryLimitMiB: 512,
+      environment: {
+        "WEBUI_AUTH": "False",
+        "OPENAI_API_KEY": "0p3n-w3bu!",
+        "OPENAI_API_BASE_URL": `http://${pipelinesBackendService.loadBalancer.loadBalancerDnsName}`,
+      },
+      logging: new ecs.AwsLogDriver({
+        logGroup,
+        streamPrefix: 'WebUI',
+      }),
+    });
+
+    webUIContainer.addPortMappings({
+      containerPort: 8080,
+    });
+    // Add a dependency so that the WebUI service only starts after the Pipelines Backend is running
+    webUIContainer.node.addDependency(pipelinesBackendService);
+
+
+    // Create the Web UI service and attach it to the load balancer
     const webUIAppService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'WebUiService', {
       cluster,
+      serviceName: 'web-ui',
       taskDefinition: webUITaskDef,
       publicLoadBalancer: true,
-      listenerPort: 80, // Make the load balancer listen on port 80
+      listenerPort: 80,
+      securityGroups: [webUISecurityGroup],
     });
 
-    const pipelinesBackendService = new ecs.FargateService(this, 'pipelinesBackendService', {
-      cluster,
-      taskDefinition: pipelinesBackendTaskDef,
-      assignPublicIp: true,
-      desiredCount: 1,
-    });
 
-    // Allow WebUI to call pipelines backend
-    pipelinesBackendService.connections.allowFrom(webUIAppService.service, ec2.Port.tcp(9099));
 
-    // Output the load balancer URL
+    // Allow WebUI to call pipelines backend (from the Web UI security group to the Pipelines Backend security group)
+    pipelinesBackendSecurityGroup.addIngressRule(webUISecurityGroup, ec2.Port.tcp(9099), 'Allow WebUI to access Pipelines Backend');
+
+    // Output the load balancer URLs
     new cdk.CfnOutput(this, 'WebUI', {
       value: `http://${webUIAppService.loadBalancer.loadBalancerDnsName}`,
+    });
+
+    new cdk.CfnOutput(this, 'PipelinesBackend', {
+      value: `http://${pipelinesBackendService.loadBalancer.loadBalancerDnsName}`,
     });
   }
 }
